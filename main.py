1d661604af108688be847073cd5c8c3f52766898
GalileoRobotics <info@galileorobotics.ch>
2020-08-13 19:23:07 +0200

    Use sockets instead of websockets

--- main.py
+++ main.py
@@ -3,23 +3,25 @@ import config
 import json
 import struct
 import websockets
+import socket
 
 
 async def send(websocket, path):
     async for _ in websocket:
         await websocket.send(json.dumps(data))
 
-
-async def receive(websocket, path):
-    ip = websocket.remote_address[0]
-    async for message in websocket:
-        data[ip] = struct.unpack("3f", message)
-
+async def receive(reader, writer):
+    message = await reader.read(1024)
+    peer = writer.get_extra_info("peername")
+    data[peer] = struct.unpack("3f", message)
+    writer.close()
 
 data = {}
 
-asyncio.get_event_loop().run_until_complete(
-    websockets.serve(receive, "0.0.0.0", config.RECEIVE_PORT))
-asyncio.get_event_loop().run_until_complete(
-    websockets.serve(send, "0.0.0.0", config.SEND_PORT))
-asyncio.get_event_loop().run_forever()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(websockets.serve(send, config.HOST, config.RECEIVE_PORT))
+receiver = asyncio.start_server(receive, config.HOST, config.RECEIVE_PORT, loop=loop)
+loop.run_until_complete(receiver)
+loop.run_forever()
+
+# TODO: Exit gracefully
