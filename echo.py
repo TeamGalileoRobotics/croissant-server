bafa309aa5f56357c38424be3509349f13cd51cc
GalileoRobotics <info@galileorobotics.ch>
2020-10-22 19:47:20 +0200

    Fix test script

--- echo.py
+++ echo.py
@@ -8,5 +8,5 @@ async def echo(websocket, path):
         await websocket.send(message)
 
 asyncio.get_event_loop().run_until_complete(
-    websockets.serve(echo, "0.0.0.0", config.PORT))
+    websockets.serve(echo, "0.0.0.0", config.SEND_PORT))
 asyncio.get_event_loop().run_forever()
